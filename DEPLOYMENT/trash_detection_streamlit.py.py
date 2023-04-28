a8b961e06de644fbcfc09cd225826aee55df6a53
GOKULPRASANTH T <121724612+Gokulprasanth-t@users.noreply.github.com>
2023-04-28 21:55:30 +0530

    Update trash_detection_streamlit.py.py

--- DEPLOYMENT/trash_detection_streamlit.py.py
+++ DEPLOYMENT/trash_detection_streamlit.py.py
@@ -6,7 +6,6 @@ Created on Fri Apr 28 13:46:37 2023
 """
 
 import os
-import json
 import cv2
 import numpy as np
 import streamlit as st
@@ -14,8 +13,24 @@ import tensorflow as tf
 from tensorflow.keras.models import load_model
 
 
-model_path = "C:/Users/gokul/Downloads/resnet.h5"
+# Set up paths and load the model
+model_path = "C:/Users/harin/Downloads/resnet.h5"
 model = load_model(model_path)
+
+def add_bg_from_url():
+    st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background-image: url("https://www.shutterstock.com/image-photo/blurred-mountain-background-260nw-269981756.jpg");
+             background-attachment: fixed;
+             background-size: cover;
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
+add_bg_from_url()
 
 # Define prediction function
 def predict(image):
@@ -28,15 +43,19 @@ def predict(image):
 
 # Define Streamlit app
 def app():
-    st.title("Object Detection")
+    st.title("𝐎𝐁𝐉𝐄𝐂𝐓 𝐃𝐄𝐓𝐄𝐂𝐓𝐈𝐎𝐍")
     st.set_option('deprecation.showfileUploaderEncoding', False)
     
     # Upload image
-    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+    uploaded_file = st.file_uploader("𝐂𝐇𝐎𝐎𝐒𝐄 𝐀𝐍 𝐈𝐌𝐀𝐆𝐄", type=["jpg", "jpeg", "png"])
     if uploaded_file is not None:
         image = cv2.imdecode(np.fromstring(uploaded_file.read(), np.uint8), cv2.IMREAD_UNCHANGED)
         st.image(image, caption='Uploaded Image', use_column_width=True)
         st.write("")
-        st.write("Classifying...")
+        st.write("𝐂𝐋𝐀𝐒𝐒𝐈𝐅𝐈𝐄𝐃")
         prediction = predict(image)
-        st.write("Prediction:", prediction)
+        predicted_class = np.argmax(prediction, axis=1)
+        st.write("Predicted Class:", predicted_class)
+
+if __name__ == "__main__":
+    app()
